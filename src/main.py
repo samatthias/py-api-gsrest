be920ae0693d5c5066fb1776753a85f7754153e3
samatthias <samatthias@m-mosimann.com>
2025-03-14 20:47:24 +0100

    working fastapi added and gitignore file to exclude venv

--- src/main.py
+++ src/main.py
@@ -1,24 +1,35 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
+from starlette.background import BackgroundTasks
 import shutil
 import os
 
 app = FastAPI()
 
 @app.post("/convert")
-async def convert_pdf(file: UploadFile = File(...)):
+async def convert_pdf(background_tasks : BackgroundTasks, file: UploadFile = File(...)):
     # Temporärer Pfad zum Speichern der hochgeladenen Datei
-    temp_file = f"temp_{file.filename}"
+    #temp_file = f"{file.filename}"
+    print(f'Content type of file: {file.content_type}')
     
-    try:
-        # Speichern der hochgeladenen Datei
-        with open(temp_file, "wb") as buffer:
-            shutil.copyfileobj(file.file, buffer)
+    # Speichern der hochgeladenen Datei
+    with open("/tmp/" + file.filename, "wb") as buffer:
+       shutil.copyfileobj(file.file, buffer)
+    print(file.filename)
+       
+    # Rückgabe der Datei als Response
+    #headers = {'Content-Disposition': 'inline; filename="out.pdf"'}
+    background_tasks.add_task(remove_file, "/tmp/" + file.filename)
+    return FileResponse("/tmp/" + file.filename, media_type="application/pdf", filename=file.filename)
         
-        # Rückgabe der Datei als Response
-        return FileResponse(temp_file, media_type="application/pdf", filename=file.filename)
+
+
+def remove_file(path: str) -> None:
+    if os.path.exists(path):
+      os.remove(path)
     
-    finally:
-        # Aufräumen: Löschen der temporären Datei
-        if os.path.exists(temp_file):
-            os.remove(temp_file)
+    #finally:
+    #    # Aufräumen: Löschen der temporären Datei
+    #    print("finally")
+    #    if os.path.exists("C:/Users/Matthias/Documents/github/tmp/" +file.filename):
+    #      os.remove("C:/Users/Matthias/Documents/github/tmp/" +file.filename)
