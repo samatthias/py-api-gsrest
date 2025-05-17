57394f3578172b2a531e58d051b622545a5aecad
samatthias <samatthias@m-mosimann.com>
2025-05-17 18:22:20 +0200

    minor changes done

--- README.md
+++ README.md
@@ -5,15 +5,21 @@ Validator used:
 https://demo.verapdf.org/
 https://www.pdf-online.com/osa/validate.aspx (End of Service announced in Jun 2024 for end of year 2025)
 
+Resources:
+https://stackoverflow.com/questions/27107923/detecting-mostly-empty-images-using-imagemagick
+https://imagemagick.org/script/identify.php
+
 
 # test call with curl against python fastapi
 curl.exe -v http://127.0.0.1:8080/convert -H "accept: application/json" -H "Content-Type: multipart/form-data" -F "file=@0001.pdf" -o o2-1.pdf
 
+curl.exe -v http://127.0.0.1:8080/readbarcode -H "Content-Type: multipart/form-data" -F "file=@0001.pdf"
+
 # build in root directory with docker/podman
-podman build -t gsapi:v4 -f .\build\Dockerfile .
+podman build -t py-api-gsrest:2025-05-17 -f .\build\Dockerfile .
 
 # run container
-podman run -p 8080:8080 -d gsapi:v4
+podman run -p 8080:8080 -d py-api-gsrest:2025-05-17
 
 # show logs std out
 podman logs <containerid>
--- src/main.py
+++ src/main.py
@@ -7,8 +7,131 @@ import subprocess
 
 app = FastAPI()
 
-@app.post("/convert")
-async def convert_pdf(background_tasks : BackgroundTasks, file: UploadFile = File(...)):
+@app.post("/mergepdf")
+async def merge_pdf_files(background_tasks : BackgroundTasks, file: UploadFile = File(...)):
+  print(f'Content type of file: {file.content_type}')
+
+  # Prepare file for processing
+  tmpDir = "/tmp/"
+  inputFileName = file.filename
+  inputFilePath = tmpDir + inputFileName
+  
+  # Save uplodaed file
+  with open(inputFilePath, "wb") as buffer:
+    shutil.copyfileobj(file.file, buffer)
+
+  command = "unzip " + inputFilePath + " -d " + tmpDir
+  print(command)
+  result = subprocess.run(command, shell=True, capture_output=True, text=True)
+  print("Return Code:", result.returncode)
+  print("Output:", result.stdout)
+
+  # qpdf --empty --pages *.pdf -- new.pdf
+
+  command = "qpdf --empty --pages /tmp/*.pdf -- /tmp/tmp.pdf"
+  print(command)
+  result = subprocess.run(command, shell=True, capture_output=True, text=True)
+  print("Return Code:", result.returncode)
+  print("Output:", result.stdout)
+
+  # cleanup /tmp/ directory
+  background_tasks.add_task(remove_file, tmpDir)
+
+  return FileResponse("/tmp/tmp.pdf", media_type="application/pdf", filename="tmp.pdf")
+
+
+
+@app.post("/metadata")
+async def readbarcode_from_pdf(background_tasks : BackgroundTasks, file: UploadFile = File(...)):
+   print(f'Content type of file: {file.content_type}')
+
+   # Prepare file for processing
+   tmpDir = "/tmp/"
+   inputFileName = file.filename
+   inputFilePath = tmpDir + inputFileName
+
+   # Save uplodaed file
+   with open(inputFilePath, "wb") as buffer:
+     shutil.copyfileobj(file.file, buffer)
+   
+   # json dict
+   return_message = {}
+   return_message['fileName'] = inputFileName
+
+
+   # convert -density 300 -crop 500x500+2050+0 2_AD.pdf jpg:- | zbarimg --nodbus --nodisplay --quiet --raw -
+   # convert 0__ADempty.pdf -format '%[mean] %[max]' info:- | awk '{print $1/$2}'
+   # convert -density 300 2_AD.pdf jpg:- | identify -ping -format '%w' -
+   # convert pdf to jpg
+
+   # read width from jpg in pdf
+   command = "convert -density 300 " + inputFilePath + " jpg:- | identify -ping -format '%w' -"
+   print(command)
+   result = subprocess.run(command, shell=True, capture_output=True, text=True)
+   print("Return Code:", result.returncode)
+   print("Output:", result.stdout)
+   return_message['width'] = result.stdout.strip()
+
+
+   # read percentage of white pixels from jpg in pdf
+   command = "convert " + inputFilePath + " -format '%[mean] %[max]' info:- | awk '{print $1/$2}'"
+   print(command)
+   result = subprocess.run(command, shell=True, capture_output=True, text=True)
+   print("Return Code:", result.returncode)
+   print("Output:", result.stdout)
+   return_message['percentageWhitePixels'] = result.stdout.strip()
+
+
+   # read barcode
+   return_message['barcode'] = "NONE"
+   calcStartpointX = int(return_message['width']) - 500
+   regionOfInterest = "500x500+" + str(calcStartpointX) +"+0"
+   # 1) convert -density 300 -crop 500x500+1980+0 0015.pdf jpg:- | zbarimg --nodbus --nodisplay --quiet --raw -
+   # 2) convert +repage -define profile:skip=icc -threshold 50% -morphology open square:1 -density 300 -crop 500x500+1980+0 in.pdf out.png
+   #
+   # -density 300 = resolution of image
+   # -crop 500x500+1980+0 = cut a rectangle from the original image width+heigth+start_x+start_y
+   # +repage =  Removes virtual canvas metadata Source: https://stackoverflow.com/questions/69689869/improve-zbarimg-qrcode-recognition
+   # -define profile:skip=icc = elimininate icc color warning from image magick Source: https://github.com/ImageMagick/ImageMagick/discussions/6292
+   # -threshold 50% = convert to binary Source: https://stackoverflow.com/questions/69689869/improve-zbarimg-qrcode-recognition
+   #
+   # -morphology open square:1 = Cleans small imperfections without altering code structure
+   # Source 1: https://github.com/mchehab/zbar/issues/123
+   # Source 2: https://stackoverflow.com/questions/69689869/improve-zbarimg-qrcode-recognition
+
+   # zbarimg improvements
+   # 1) zbarimg --nodbus --nodisplay --quiet --raw in.jpg
+   # 
+   # -Sdisable = turns off all decoders
+   # Sources: https://kb.offsec.nl/tools/forensics/zbar-tools/
+   # http://c3codes.ourproject.org/C3_Codes_Tutorial.pdf
+
+   # -Sqrcode.enable = re-enables only QR codes
+   # Sources: https://kb.offsec.nl/tools/forensics/zbar-tools/
+   # https://manpages.ubuntu.com/manpages/bionic/man1/zbarimg.1.html
+   #
+   # Avoid JPG artifacts: Use PNG for intermediate processing to prevent quality loss
+   #
+   # 
+
+
+   command = "convert +repage -define profile:skip=icc -threshold 50% -morphology open square:1 -density 300 -crop " + regionOfInterest + " " + inputFilePath + " jpg:- | zbarimg -Sdisable -Sqrcode.enable --nodbus --nodisplay --quiet --raw -"
+   print(command)
+   result = subprocess.run(command, shell=True, capture_output=True, text=True)
+   print("Return Code:", result.returncode)
+   print("Output:", result.stdout)
+   if (result.stdout.strip()):
+    return_message['barcode'] = result.stdout.strip()
+
+    
+
+    # remove file after return the json message
+    background_tasks.add_task(remove_file, tmpDir)
+   return return_message
+   
+
+@app.post("/convertpdfa")
+async def convert_pdfa(background_tasks : BackgroundTasks, file: UploadFile = File(...)):
     # Temporärer Pfad zum Speichern der hochgeladenen Datei
     #temp_file = f"{file.filename}"
     print(f'Content type of file: {file.content_type}')
@@ -37,18 +160,13 @@ async def convert_pdf(background_tasks : BackgroundTasks, file: UploadFile = Fil
        
     # Rückgabe der Datei als Response
     #headers = {'Content-Disposition': 'inline; filename="out.pdf"'}
-    background_tasks.add_task(remove_file, "/tmp/" + inputFileName)
-    background_tasks.add_task(remove_file, "/tmp/" + outputFileName)
+    background_tasks.add_task(remove_file, "/tmp/")
     return FileResponse("/tmp/" + outputFileName, media_type="application/pdf", filename=outputFileName)
         
 
 
-def remove_file(path: str) -> None:
-    if os.path.exists(path):
-      os.remove(path)
+def remove_file(dir: str) -> None:
+  for root, dirs, files in os.walk(dir):
+    for file in files:
+        os.remove(os.path.join(root, file))
     
-    #finally:
-    #    # Aufräumen: Löschen der temporären Datei
-    #    print("finally")
-    #    if os.path.exists("C:/Users/Matthias/Documents/github/tmp/" +file.filename):
-    #      os.remove("C:/Users/Matthias/Documents/github/tmp/" +file.filename)
